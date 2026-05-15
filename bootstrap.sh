d36f3d9eb9a83588ef9abb89ca164be9c65cb1ce
Nick Taylor <nick@nickyt.co>
2026-05-15 17:12:39 -0400

    chore: improved ascii art

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,13 +42,13 @@ API_TOKENS_URL="https://console.pomerium.app/app/management/api-tokens"
 banner() {
   cat <<'EOF'
 
-   ____             ____ _
-  |  _ \ ___  _ __ / ___| | __ ___      __
-  | |_) / _ \| '_ \ |   | |/ _` \ \ /\ / /
-  |  __/ (_) | | | | |__ | | (_| |\ V  V /
-  |_|   \___/|_| |_|\____|_|\__,_| \_/\_/
+  ____                  ____ _
+ |  _ \ ___  _ __ ___  / ___| | __ ___      __
+ | |_) / _ \| '_ ` _ \| |   | |/ _` \ \ /\ / /
+ |  __/ (_) | | | | | | |___| | (_| |\ V  V /
+ |_|   \___/|_| |_| |_|\____|_|\__,_| \_/\_/
 
-  Pomerium + OpenClaw deployment bootstrap
+ Pomerium + OpenClaw deployment bootstrap
 
 EOF
 }
